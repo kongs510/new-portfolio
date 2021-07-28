9c188b358c41952c4ff242db7dcc128b6186ab45
공민철 <68903200+kongs510@users.noreply.github.com>
2021-07-28 13:47:08 +0900

    이미지파일 경로 재수정

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -11,7 +11,7 @@ function Nav() {
     <section className="Header">
       <nav>
         <button className="btn" id="logo-wrapper" onClick={scrollTop}>
-          <img id="logo-image" src="/home.png" alt="페이지 맨 위로 이동하기" />
+          <img id="logo-image" src="/img/home.png" alt="페이지 맨 위로 이동하기" />
         </button>
         <a
           href="https://drive.google.com/file/d/1VnjRsdQiLzxdUWIlpcSaF1OoieyHhLud/view?usp=sharing"
